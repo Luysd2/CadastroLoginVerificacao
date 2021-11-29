f95e7ed1077ecb47023c9324aa6264cd5e99c59e
Luis Sousa <luysd2@gmail.com>
2021-11-29 13:20:28 -0300

    Adicionei um session_regenerate_id() para que quando a pagina for atualizada, o id da pagina mude, aumentando a segurança do usuario

--- painel.php
+++ painel.php
@@ -2,6 +2,11 @@
 session_start();
 include('verifica_login.php');
 
+session_regenerate_id(); 
+//para mudar o id da seção cada vez q atualizar a pagina
+//por padrão o id é o msm, msm quando vc atualiza a pagina
+
+
 ?>
 <h2>Olá, <?php  echo $_SESSION['usuario'];?></h2>
 <h2><a href="sair.php">Sair</a></h2>
